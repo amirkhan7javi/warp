bbe1b45ea59a8f10e7e6bbb9d80919a0c0235a8d
Swapnil Raj <raj.swapnil99@gmail.com>
2022-06-28 18:18:02 +0100

    Add semantic tests for solidity 0.7.6

--- tests/behaviour/expectations/semantic_exceptions.ts
+++ tests/behaviour/expectations/semantic_exceptions.ts
@@ -1,4 +1,4 @@
 export const exceptions: string[] = [
   //Out of bounds arguments are passed incorrectly
-  'tests/behaviour/solidity/test/libsolidity/semanticTests/arithmetics/checked_add_v2',
+  'tests/behaviour/8/solidity/test/libsolidity/semanticTests/arithmetics/checked_add_v2',
 ];
